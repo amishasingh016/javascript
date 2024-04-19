a7d1e40f306b71a64b610ce6670bbfb0ad9f093c
amishasingh016 <itsamisha011@gmail.com>
2024-04-20 04:53:11 +0530

    dateandtime

---
+++ 04_dateandtime.js
@@ -0,0 +1,10 @@
+const myDate=new Date(2002,4,0,6,9)
+console.log(myDate.toLocaleString())
+
+let myTimestamp=Date.now()
+console.log(myTimestamp)
+
+console.log(Math.floor(Date.now()/1000))
+console.log(typeof myDate)
+
+console.log(myDate.getMonth())
