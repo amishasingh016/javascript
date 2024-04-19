e8259e5c2a92c9ec87a5e7cae8f776ad2d611ecc
amishasingh016 <itsamisha011@gmail.com>
2024-04-20 04:34:54 +0530

    nums

---
+++ numsandmath.js
@@ -0,0 +1,12 @@
+//NUMBER
+
+const balance=new Number(3000000)
+console.log(balance)
+console.log(balance.toLocaleString())
+
+
+//math
+
+console.log(Math.abs(-55))
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
