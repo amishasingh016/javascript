3423b484e149c37187a3b9bf89e5102741f1ccfa
amishasingh016 <itsamisha011@gmail.com>
2024-04-21 09:26:12 +0530

    scope

---
+++ 08_scopes.js
@@ -0,0 +1,20 @@
+if(true){
+    let a=10;
+    const b=20;
+    //console.log("INNER");
+}
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
